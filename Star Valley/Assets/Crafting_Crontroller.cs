9531a2f9538fe8f64694c41da9e1f90d09e02846
Joel <nintendogamer249@gmail.com>
2018-02-09 17:51:40 -0400

    Added Test Scripts
    Learned About Scriptable Objects And I've Begun Adding Scripts For testing The Crafting,Recipe And Component Systems

---
+++ Star Valley/Assets/Crafting_Crontroller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crafting_Crontroller : MonoBehaviour {
+
+	public recipeCreator recipe;
+	void Start () {
+		Debug.Log(recipe);
+	}
+	
+	void Update () {
+		
+	}
+}
---
+++ Star Valley/Assets/RecipeCreator.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Recipe", menuName = "Create Recipe")]
+public class recipeCreator : ScriptableObject {
+	//Add Ability To Add More Components
+
+	public string Component1;
+	public string Component2;
+
+	public string Result;
+	void Update ()
+	{
+
+	}
+
+}
