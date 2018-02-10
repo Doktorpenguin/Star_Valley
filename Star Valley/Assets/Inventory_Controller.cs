0bfac1994eac700ea28d581576720dc65329cd6b
Joel <nintendogamer249@gmail.com>
2018-02-10 00:19:59 -0400

    new Commit 2/10/18

--- Star Valley/Assets/Inventory_Controller.cs
+++ Star Valley/Assets/Inventory_Controller.cs
@@ -6,7 +6,8 @@ public class Inventory_Controller : MonoBehaviour {
 
 	public string[] Inventory = new string[10];
 	void Start () {
-		
+		Inventory[0] = "Health";
+		Inventory[1] = "Damage";
 	}
 	
 	void Update () {
